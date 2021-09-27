3df486e7118184e6d21df49c80bf1ebe6a025079
Comajakit <81400328+Comajakit@users.noreply.github.com>
2021-09-27 19:38:43 +0700

    Add files via upload

---
+++ Q1.1/pd.py
@@ -0,0 +1,21 @@
+#Producer
+from time import sleep
+from json import dumps
+import datetime
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+
+f = open(r"C:\kafka_2.13-2.7.0\book.txt", "r",encoding="utf8")
+
+
+
+
+
+for i in f:
+    data = i
+    print(data)
+    producer.send('streams-plaintext-input', value=data)
+    #sleep(1)
